79b04c1c4538205fc9beba819ecffd40e9e64f6b
Silvio Luiz <silvio.luiz@gmail.com>
2018-09-10 23:04:19 -0300

    Provinces (#5)
    * Arquivo de validacao de params da busca de propriedade
    
    * Inclusao de arquivo com os boundaries das provincias
    
    * Inclusao de funcao de carregamento de arquivos
    
    * Inclusao de modelo da entidade Provincia e ajustes em Property
    
    * Inclusao de operacoes relacionadas a busca de propriedades
    
    * Inclusao de metodos relacionados a busca de properties, melhorias no tratamento de excecao e responses

--- app/properties/models.py
+++ app/properties/models.py
@@ -1,4 +1,6 @@
 from app import db
+from .utils import load
+import json
 
 class Property(db.Model):
     __tablename__ = 'properties'
@@ -12,9 +14,25 @@ class Property(db.Model):
     beds = db.Column(db.Integer, nullable=False)
     baths = db.Column(db.Integer, nullable=False)
     squareMeters = db.Column(db.Integer, nullable=False)
+    provinces = []
     
     def as_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        colum_list = {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        colum_list['provinces'] = self.provinces 
+        return colum_list
 
     def __str__(self):
         return str(self.__class__) + ": " + str(self.__dict__)
+
+class Province():
+    
+    def find_by_x_y(x, y):
+        provinces = load('schemas/provinces.json')
+        founded_provinces = [ k for k,v in provinces.items() 
+            if(x >= v['boundaries']['upperLeft']['x'] and 
+               x <= v['boundaries']['bottomRight']['x'] and
+               y <= v['boundaries']['upperLeft']['y'] and 
+               y >= v['boundaries']['bottomRight']['y']
+            ) 
+        ]
+        return founded_provinces
--- app/properties/routes.py
+++ app/properties/routes.py
@@ -1,4 +1,3 @@
-
 import os
 import json
 from flask import request, abort, jsonify, make_response, Response
@@ -7,13 +6,14 @@ from jsonschema import validate, ErrorTree, Draft4Validator as Validator
 from app.properties import properties_bp
 from .models import Property
 from .repositories import PropertyRepository as Repository
+from .utils import load
 
 @properties_bp.route('/properties', methods=['POST'])
 def create_properties():
     if (not request.is_json):
         abort(make_response(jsonify(message='Payload or mime type is not valid'), 415))
     else:
-        errors = Validator(_load_schema('property.schema')).iter_errors(request.get_json())
+        errors = Validator(load('schemas/property.schema')).iter_errors(request.get_json())
         response_error = _errors(errors)
         if (response_error):
             resp = Response(response_error,
@@ -31,22 +31,39 @@ def find_property(id):
     if (not prop):
         message = 'Property id {} not found'.format(id)
         abort(make_response(jsonify(message=message), 404))
-    return jsonify(prop.as_dict()), 200;
+    return jsonify(prop.as_dict()), 200
 
 @properties_bp.route('/properties')
 def search_properties():
-    pass
+    
+    upper_x = request.args['ax']
+    upper_y = request.args['ay']
+    bottom_x = request.args['bx']
+    bottom_y = request.args['by']
+    
+    params_json = '{"ax":%s, "ay":%s, "bx":%s, "by":%s  }' % (upper_x, upper_y, bottom_x, bottom_y)
+    errors = Validator(load('schemas/filter.schema')).iter_errors(json.loads(params_json))
+    
+    response_error = _errors(errors)
+    if (response_error):
+        resp = Response(response_error,
+            status=422,
+            mimetype="application/json")
+        return resp
+    
+    result = Repository.find_properties(upper_x, bottom_x, bottom_y, upper_y)
+    if(not result):
+        message = 'No properties found with these coordinates'
+        abort(make_response(jsonify(message=message), 404))
+    else:
+        response = '{ "foundProperties": %s, "properties": %s }' % (len(result), json.dumps(result, ensure_ascii=False)) #, )
+        return Response(response,
+                status=200,
+                mimetype="application/json")
 
 @properties_bp.errorhandler(400)
 def bad_request(e):
     return jsonify(error=400, text=str(e)), 400
-    
-def _load_schema(filename):
-    dir = os.path.dirname(__file__)
-    file_path = os.path.join(dir, ('schemas/%s' % filename))
-    with open(file_path, 'r') as schema_file:
-        data = json.load(schema_file)
-    return data
 
 def _errors(errors):
     lista = []
